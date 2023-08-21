c6575986ee17ba681b2f253e1ad0d37c2a15b597
edemyashina <dem-ea@yandex.ru>
2023-08-21 23:36:29 +0300

    first

---
+++ script.mjs
@@ -0,0 +1,12 @@
+// const a=require('mpackage')
+import {a} from 'mpackage'
+console.log(a)
+const user={
+    
+  name: 'максим лескин',
+  dateBirth: '10.03.1987',
+  purpose: 'карьерный рост'
+
+}
+const retVal=a(user)
+console.log(retVal)
